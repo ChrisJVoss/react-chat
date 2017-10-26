e0556eebec57a86e69cf2ff88c80d791ca7c5c07
ChrisJVoss <chrisvoss92@gmail.com>
2017-10-26 14:47:59 -0700

    Socket is now exposed and ready to be used.

--- tools/server.js
+++ tools/server.js
@@ -1,13 +1,12 @@
-import express from 'express';
-import webpack from 'webpack';
-import path from 'path';
-import config from '../webpack.config.dev';
-import open from 'open';
+import express from 'express'
+import webpack from 'webpack'
+import path from 'path'
+import config from '../webpack.config.dev'
+import open from 'open'
 import socket from 'socket.io'
 import { Server } from 'http'
 
-
-const port = 3000;
+const port = 3000
 const app = express()
 const server = Server(app)
 const compiler = webpack(config)
@@ -16,13 +15,13 @@ const io = socket(server)
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true, publicPath: config.out.publicPath
-}));
+}))
 
-app.use(require('webpack-hot-middleware')(compiler));
+app.use(require('webpack-hot-middleware')(compiler))
 
 app.use('*', function(req, res) {
- res.sendFile(path.join(__dirname, '../src/index.html'));
-});
+ res.sendFile(path.join(__dirname, '../src/index.html'))
+})
 
 io.on('connection', function(socket) {
   console.log('a user connected')
@@ -30,8 +29,8 @@ io.on('connection', function(socket) {
 
 server.listen(port, function(err) {
   if (err) {
-    console.log('error', err);
+    console.log('error', err)
    } else {
-      open(`http://localhost:${port}`);
+      open(`http://localhost:${port}`)
    }
- });
+ })
