c5c281150bbc05bd55662a999a047921f94771cf
ChrisJVoss <chrisvoss92@gmail.com>
2017-10-27 15:42:26 -0700

    Setting up webpack.

---
+++ src/components/webpack.config.dev.js
@@ -0,0 +1,12 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+  debug: true,
+  devtool: 'cheap-module-eval-source-map',
+  noInfo: false,
+  entry: [
+    'eventsource-polyfill',
+    'webpack-hot-middleware/client?reload=true',
+    './src/index'
+  ],
