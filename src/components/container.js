5bc67830aa6cb528227db98a922760ba546a4cc3
ChrisJVoss <chrisvoss92@gmail.com>
2017-11-03 22:26:26 -0700

    Component to handle add messages.

--- src/components/container.js
+++ src/components/container.js
@@ -179,3 +179,17 @@ class NoteList extends React.Component {
     </ul>
   }
 }
+class AddNote extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleSubmit(event) {
+    const postNote = (newNote) => {
+      let fetchData = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newNote)
+      }
